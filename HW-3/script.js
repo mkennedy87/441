df072128608b1de577e8552dcf6a154abc467d5f
Mike <46776723+mkennedy87@users.noreply.github.com>
2024-02-05 21:39:20 -0700

    can't figure out why i can't change the dom

--- HW-3/script.js
+++ HW-3/script.js
@@ -1,9 +1,62 @@
-let a = 1;
-let b = 2;
-let c = 3;
+var a = 0;
+var b = 0;
+var c =0;
+//let img1 = ;
+//let img2 = ;
 
-function myFunction()
+
+function disney()
 {
-    window.alert("Squidward smells");
+    a++;
+    window.alert("Memories");
 }
 
+function dreamWorks()
+{
+   //a--
+    window.alert("Memes");
+    a--;
+}
+
+function lamp()
+{
+    b++;
+    window.alert("You ain't never had a friend like me!");
+}
+
+function prideRock()
+{
+    b--;
+    window.alert("Hakunamatata!");
+}
+
+function swamp()
+{
+    c++;
+    window.alert("hubbahubba!");
+}
+
+function castle()
+{
+    c--;
+    window.alert("You lose!");
+}
+
+if ( a >= 1, b = 0, c = 0 )
+{
+    document.getElementById("img1").innerHTML = <img src="./img/Disney/lamp.jpg" width="200">;
+    document.getElementByID("top").innerHTML = "Lamp";
+}
+else if ( a <= -1, b = 0, c = 0 ) 
+{
+    document.getElementById("img1").innerHTML = <img src="./img/DreamWorks/castle.jpg" width="200">;
+    document.getElementByID("top").innerHTML = "Castle";
+}
+
+/*
+else if ( a >= 1, b >= 1, c = 0 ) 
+{
+    document.getElementById("img1").innerHTML = <img src="./img/DreamWorks/dragon.jpg" width="200">;
+    document.getElementByID("top").innerHTML = "dragon";
+}
+*/
