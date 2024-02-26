9fd43247d611b0fdba3d15c26e2c237301b4e9f3
Mike <46776723+mkennedy87@users.noreply.github.com>
2024-02-26 13:27:02 -0700

    Finished assignment

--- HW-6/main.js
+++ HW-6/main.js
@@ -1,13 +1,21 @@
-var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12"];
+var imageNames = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12"];
 var blankImagePath = "images/fish.jpeg";
+
+var firstNumber = -1;
+var secondNumber = -1;
+var Attempts = 0;
+var allFound = 0;
+// JSON declaration
+var player = {"firstname":"", "lastname":"", "age":0, "Attempts":0};
+
 var actualImages = new Array();
 
 function printBlanks()
 {
     createRandomImageArray();
-    for(var i = 0; i < imageTags.length; i++)
+    for(var i = 0; i < imageNames.length; i++)
     {
-        document.getElementById(imageTags[i]).src= blankImagePath;
+        document.getElementById(imageNames[i]).src= blankImagePath;
     }
        
 }
@@ -36,6 +44,79 @@ function createRandomImageArray()
 
 function flipImage(number)
 {
-    document.getElementById(imageTags[number]).src= actualImages[number];
+    //document.getElementById(imageTags[number]).src= actualImages[number];
+  // make the second image appear
+  if(firstNumber >= 0)
+  {
+      secondNumber = number;
+      document.getElementById(imageNames[number]).src = actualImages[secondNumber];
+      
+  }
+  else if(firstNumber < 0) // make the first image appear
+  {
+      firstNumber = number;
+      document.getElementById(imageNames[firstNumber]).src= actualImages[firstNumber];
+  
+  }
 
+  // check to see if the images do not match
+  if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+  {
+      Attempts++;
+      setTimeout(imagesDisappear, 1000); // calls a method after 1 second
+  }
+  // check to see if the images do match
+  else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+  {
+      Attempts++;
+      allFound++;
+      
+      firstNumber = -1;
+      secondNumber = -1;
+      if(allFound == actualImages.length/2)
+      {  
+          player.Attempts = Attempts;
+          localStorage.setItem("playerInfo", JSON.stringify(player));
+          window.location = "end.html";
+      }
+  }
+}
+
+function imagesDisappear()
+{
+
+    console.log(firstNumber);
+    document.getElementById(imageNames[firstNumber]).src = blankImagePath;
+    document.getElementById(imageNames[secondNumber]).src = blankImagePath;
+    firstNumber = -1;
+    secondNumber = -1;
+}
+
+function addToPlayer()
+{
+    var firstName = document.getElementById("txtFirstName").value;
+    var lastName = document.getElementById("txtLastName").value;
+    var age = document.getElementById("txtAge").value;
+   
+    player.firstname = firstName;
+    player.lastname = lastName;
+    player.age = age;
+    localStorage.setItem("playerInfo", JSON.stringify(player));
+    window.location = "index.html";
+}
+
+// get the information out of JSON
+function playerInfo()
+{
+    var playerInformation = localStorage.getItem("playerInfo");
+    player = JSON.parse(playerInformation);
+    var str = "Name: " + player.firstname + " " + player.lastname + "<br>" +
+    "Age: " + player.age + "<br>" +
+    "Attempts: " + player.Attempts;
+    if(document.getElementById("endInformation") != null)
+    {
+        document.getElementById("endInformation").innerHTML = str;
+    }
+    
+   
 }
